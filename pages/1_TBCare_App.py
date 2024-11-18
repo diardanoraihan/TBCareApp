f6f9a241c93f135b7c4f4b5003883b0a271c4d62
Diardano Raihan <diardano.raihan@sekolahmu.co.id>
2024-11-18 11:33:15 +0700

    hide streamlit main menu in header and footer

--- pages/1_TBCare_App.py
+++ pages/1_TBCare_App.py
@@ -353,11 +353,11 @@ with st.spinner("Generating insights..."):
         st.error(f"An error occurred: {e}")
 
 # ---- HIDE STREAMLIT STYLE ----
-# hide_st_style = """
-#             <style>
-#             #MainMenu {visibility: hidden;}
-#             footer {visibility: hidden;}
-#             header {visibility: hidden;}
-#             </style>
-#             """
-# st.markdown(hide_st_style, unsafe_allow_html=True)
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
